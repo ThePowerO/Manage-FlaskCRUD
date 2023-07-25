79347edcb3d0d35f33d0acd0a26de291bf4d2918
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-24 22:04:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,4 @@ def add_user():
         cur.execute('INSERT INTO users (이름, 나이, 거리, 도시, 전화_번호, 이메일) values (?,?,?,?,?,?)', (이름, 나이, 거리, 도시, 전화_번호, 이메일, id))
         con.commit()
         flash("데이터 등록했어요", "성공")
+        return redirect(url_for("index"))
