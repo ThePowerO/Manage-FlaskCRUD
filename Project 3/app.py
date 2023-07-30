a490f30c95a9ad4da7fd62019a5a376377e16406
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-30 18:14:50 -0300

    Update app.py

--- Project 3/app.py
+++ Project 3/app.py
@@ -9,3 +9,22 @@ def index():
     con.row_factory = sql.Row
     cur = con.cursor()
     cur.execute("SELECT * FROM products")
+    data = cur.fetchall()
+    return render_template('index.html', datas=data)
+
+@app.route('/add_products', methods=['POST', 'GET'])
+def add_products():
+    if request.method == 'POST':
+        Product_Name = request.form['Product_Name']
+        Type = request.form['Type']
+        Brand = request.form['Brand']
+        Price = request.form['Price']
+        MadeIn = request.form['MadeIn']
+
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO products (Product_Name, Type, Brand, Price, MadeIn) values (?,?,?,?,?)", (Product_Name, Type, Brand, Price, MadeIn))
+        con.commit()
+        flash("Product Added", "success")
+        return redirect(url_for('index'))
+    return render_template('add_product.html')
