435dccd7571f9ca05f585ec58f1ac91c3b9d05b5
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-31 16:32:08 -0300

    Update app.py

--- Project 3/app.py
+++ Project 3/app.py
@@ -41,3 +41,4 @@ def edit_product(id):
         con = sql.connect('database.db')
         cur = con.cursor()
         cur.execute("UPDATE products SET Product_Name=?, Type=?, Brand=?, Price=?, MadeIn=? WHERE ID=?", (Product_Name, Type, Brand, Price, MadeIn, id))
+        con.commit()
